c79657b6f730c2133862cbd1f5a6cd1efb241a23
Yoppippi <yottiii0111@gmail.com>
2020-11-22 21:19:30 +0900

    性格診断の実装

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -2,6 +2,7 @@ class MatchingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    @character = Character.where(extra: 3)
     @favorite = Favorite.all
   end
 end
